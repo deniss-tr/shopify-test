4bb93bbc2a680126d78b6b33355b74ee02b97c86
deniss-tr <deniss.tarasevics@scandiweb.com>
2023-09-14 20:44:54 +0300

    TASK1 - implemented basic API request with simple output

---
+++ app/Controllers/BaseController.php
@@ -0,0 +1,49 @@
+<?php
+namespace Controllers;
+
+require 'vendor/autoload.php';
+
+use GuzzleHttp\Client;
+
+abstract class BaseController {
+    private const CONFIG_LOCATION = './app/etc/config.php';
+
+    public const STORE_BASE_URL = 'https://deniss-tarasevics.myshopify.com';
+
+    public function __construct()
+    {
+        $this->config = $this->loadConfig();
+        $this->client = $this->initGuzzleClient();
+    }
+
+    /**
+     * @return mixed|null
+     */
+    private function loadConfig()
+    {
+        $config = null;
+
+        if (file_exists(self::CONFIG_LOCATION)) {
+            $config = require self::CONFIG_LOCATION;
+        }
+
+        return $config;
+    }
+
+    /**
+     * @return Client|null
+     */
+    private function initGuzzleClient() {
+        if (!$this->config) {
+            return null;
+        }
+
+        return new Client([
+            'base_uri' => self::STORE_BASE_URL,
+            'headers' => [
+                'Content-Type' => 'application/json',
+                'X-Shopify-Access-Token' => $this->config['shopify_access_token'],
+            ],
+        ]);
+    }
+}
